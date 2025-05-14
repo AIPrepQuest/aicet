c02edce369148113d5554289aeac66d2f0f62a15
ramanareddyg <ramanareddyg@gmail.com>
2025-05-14 13:55:23 +0530

    initial testing

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def analyze_data(filepath):
+
+    #fill the code 
+
+    #return a dictionary with the following keys
+    # 'num_rows': number of rows in the DataFrame
+    # 'num_columns': number of columns in the DataFrame
+    # 'column_with_max_mean': name of the column with the maximum mean value;
